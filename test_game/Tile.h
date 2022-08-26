d8e35a7921e13a3f5476ade03b14d21db3b57af9
vernik03 <vernik03@gmail.com>
2022-08-27 01:56:20 +0300

    HeadSprite

--- test_game/Tile.h
+++ test_game/Tile.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include "Framework.h"
+#include "HeadSprite.h"
 
 enum class TileType {
 	intact,
@@ -21,7 +22,7 @@ enum class TileColor {
 	brown
 };
 
-class Tile
+class Tile : public HeadSprite
 {
 public:
 	Tile(TileType type, TileColor color, double new_x = 0, double new_y = 0) {
@@ -36,54 +37,9 @@ public:
 		SetXY(new_x, new_y);
 	}
 
-	void draw() {
-		drawSprite(sprite, x, y);
-	}
-
-	int getWidth() {
-		return width;
-	}
-
-	int getHeight() {
-		return height;
-	}
-
-	void setSize(int w, int h) {
-		//setSpriteSize(sprite, w, h);
-	}
-
-	double getX() {
-		return x;
-	}
-
-	double getY() {
-		return y;
-	}
-
-	void SetX(double new_x) {
-		x = new_x;
-	}
-
-	void SetY(double new_y) {
-		y = new_y;
-	}
-
-	void SetXY(double new_x, double new_y) {
-		x = new_x;
-		y = new_y;
-	}
-
 protected:
 	std::string str(TileType type);
 	std::string str(TileColor color);
-
-	Sprite* sprite;
-
-	int width;
-	int height;
-
-	double x;
-	double y;
 };
 
 
